ed13049d08d71fba632cc59b4686820be79ce90f
Strange-Quark-007 <masteryasin@ymail.com>
2024-04-25 21:48:13 +0530

    add script to check & list mods available and not available for the version

---
+++ scripts/check-mods.js
@@ -0,0 +1,41 @@
+const axios = require('axios');
+const fs = require('fs');
+require('dotenv').config();
+const ModsConfig = require('../ModsConfig.json');
+const { getFollowedProjectsModrinth, getCollectionProjects } = require('./getProjectsList');
+
+
+const checkMods = async () => {
+
+  let modsAvailable = [];
+  let modsNotAvailable = [];
+  const { gameVersion, loader, modrinthCollectionId } = ModsConfig;
+
+  const projectList = modrinthCollectionId !== '' ? await getCollectionProjects() : await getFollowedProjectsModrinth();
+
+  const allPromises = projectList?.map(project => {
+    return axios.get(`https://api.modrinth.com/v2/project/${project.Project_ID}/version?game_versions=["${gameVersion}"]&loaders=["${loader}"]`)
+      .then(res => {
+        try {
+          const fileName = res.data[0].files[0].filename;
+          const fileURL = res.data[0].files[0].url;
+          modsAvailable.push(project.Mod_Name);
+        }
+        catch (error) {
+          modsNotAvailable.push(project.Mod_Name);
+        };
+      });
+  });
+
+  Promise.all(allPromises).then(() => {
+    console.log(`Mods available for ${gameVersion}`);
+    console.log(modsAvailable);
+
+    console.log(`Mods not available for ${gameVersion}`);
+    console.log(modsNotAvailable);
+  });
+
+
+};
+
+checkMods();
